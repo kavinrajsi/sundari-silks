0db3cd43a4fe24c0f7b93b149b80514d280d073f
kavinrajsi <sikavinraj@gmail.com>
2022-09-07 01:41:44 +0530

    working on menu

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function(){
+  return gulp.src('styles/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('styles/**/*.scss', gulp.series('sass'));
+});
