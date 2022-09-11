bf5a58dc776895c22352ff81aa00ce352a044666
kavinrajsi <sikavinraj@gmail.com>
2022-09-12 01:26:16 +0530

    working on home page

---
+++ assets/global.js
@@ -0,0 +1,24 @@
+(function ($) {
+  $(function () {
+    //document.ready
+    //document.ready
+
+    //DOM READY code here
+
+  });
+
+    jQuery(".owl-carousel").owlCarousel({
+      loop: true,
+      margin: 10,
+      responsiveClass: true,
+      center: true,
+      items: 1,
+      nav: true,
+      autoplay: true,
+      autoplayTimeout: 1000,
+      autoplayHoverPause: true,
+    });
+
+  //Functions, Plugins, Etc.. Here
+  //(does not wait for DOM READY STATE)
+})(jQuery);
