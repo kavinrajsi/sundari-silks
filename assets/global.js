4b20b26978b283ff7bf88e1454849db64ce628af
kavinrajsi <sikavinraj@gmail.com>
2022-09-29 09:28:51 +0530

    working on spotlighr

--- assets/global.js
+++ assets/global.js
@@ -51,50 +51,13 @@
     $(this).children(".cd-secondary-nav").removeClass("active");
   });
 
-  function init() {
-    cacheSelectors();
-    $(selectors.siteNavHasDropdown).on("mouseenter", function () {
-      var $el = $(this);
-      showDropdown($el);
-    });
-    $(".site-nav__dropdown").on("mouseenter", function () {
-      $(this).show();
-      $(this).parent().addClass(config.activeClass);
-    });
-    $(selectors.siteNavHasDropdown).on("mouseleave", function () {
-      hideDropdown(cache.$activeDropdown);
-    });
-    $(".site-nav__dropdown").on("mouseleave", function () {
-      hideDropdown(cache.$activeDropdown);
-      $(this).hide();
-    });
-    cache.$subMenuLinks.on("click.siteNav", function (evt) {
-      // Prevent click on body from firing instead of link
-      evt.stopImmediatePropagation();
-    });
-  }
-  function showDropdown($el) {
-    $el.addClass(config.activeClass);
-    var headerHeight = $("#shopify-section-header").outerHeight(),
-      headerNav = $el.find(".site-nav__dropdown").outerHeight();
-    $el.find(".site-nav__dropdown").css({ top: +headerHeight + "px" });
-    cache.$activeDropdown = $el;
-  }
-  function hideDropdown($el) {
-    // remove aria on open dropdown
-    $el.removeClass(config.activeClass);
-    // reset active dropdown
-    cache.$activeDropdown = $(selectors.siteNavActiveDropdown);
-    $(selectors.body).off("click.siteNav");
-    $(window).off("keyup.siteNav");
-  }
 
   /**
    * Home page owl slider
    */
   jQuery(".owl-carousel").owlCarousel({
     loop: true,
-    margin: 0,
+    margin: 10,
     responsiveClass: true,
     center: true,
     items: 1,
