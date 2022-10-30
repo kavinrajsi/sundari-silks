4451a8bbb9a674ab12924980793a1d1a67ec4fe8
kavinrajsi <sikavinraj@gmail.com>
2022-10-30 22:14:43 +0530

    fixed filter scroll

--- assets/filter.js
+++ assets/filter.js
@@ -4,6 +4,11 @@ const details = document.querySelectorAll("details");
 // Add the onclick listeners.
 details.forEach((targetDetail) => {
   targetDetail.addEventListener("click", () => {
+    var filterWrapper = $(".filter--content").height();
+    // var filterContent = $('.filter--content').height();
+    console.log(filterWrapper);
+    $(".filter-group-display").css("max-height", filterWrapper);
+
     // Close all the details that are not targetDetail.
     details.forEach((detail) => {
       if (detail !== targetDetail) {
@@ -13,36 +18,30 @@ details.forEach((targetDetail) => {
   });
 });
 
-// Shopify.queryParams = {};
+Shopify.queryParams = {};
 
-// // Preserve existing query parameters
-// if (location.search.length) {
-//   var params = location.search.substr(1).split('&');
+// Preserve existing query parameters
+if (location.search.length) {
+  var params = location.search.substr(1).split("&");
 
-//   for (var i = 0; i < params.length; i++) {
-//     var keyValue = params[i].split('=');
+  for (var i = 0; i < params.length; i++) {
+    var keyValue = params[i].split("=");
 
-//     if (keyValue.length) {
-//       Shopify.queryParams[decodeURIComponent(keyValue[0])] = decodeURIComponent(keyValue[1]);
-//     }
-//   }
-// }
+    if (keyValue.length) {
+      Shopify.queryParams[decodeURIComponent(keyValue[0])] = decodeURIComponent(
+        keyValue[1]
+      );
+    }
+  }
+}
 
-// // Update sort_by query parameter on select change
-// document.querySelector('#sort-by').addEventListener('change', function (e) {
-//   var value = e.target.value;
+// Update sort_by query parameter on select change
+document.querySelector("#sort-by").addEventListener("change", function (e) {
+  var value = e.target.value;
 
-//   Shopify.queryParams.sort_by = value;
-//   location.search = new URLSearchParams(Shopify.queryParams).toString();
-// });
-
-// // Update sort_by query parameter on select change
-// document.querySelector('#sort-byFitlerMobile').addEventListener('change', function (e) {
-//   var value = e.target.value;
-
-//   Shopify.queryParams.sort_by = value;
-//   location.search = new URLSearchParams(Shopify.queryParams).toString();
-// });
+  Shopify.queryParams.sort_by = value;
+  location.search = new URLSearchParams(Shopify.queryParams).toString();
+});
 
 $("#filterButton, #filterClose").click(function () {
   $(".meFilter").toggle();
