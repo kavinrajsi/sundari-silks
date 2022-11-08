385d67803112ae68ae2456a85cc5f4fc59756170
kavinrajsi <sikavinraj@gmail.com>
2022-11-08 13:17:01 +0530

    updated the explore all card

--- assets/global.js
+++ assets/global.js
@@ -24,6 +24,29 @@
         touchMove: true,
         swipeToSlide: true,
         adaptiveHeight: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 300,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          }
+        ]
       });
 
     $(".tab-buttton .tab").click(function () {
@@ -49,6 +72,31 @@
           touchMove: true,
           swipeToSlide: true,
           adaptiveHeight: true,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
         });
     });
 
