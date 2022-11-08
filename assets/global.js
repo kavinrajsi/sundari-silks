43fab6bade4a9c31290c7cf81e30816c121391da
kavinrajsi <sikavinraj@gmail.com>
2022-11-08 15:11:56 +0530

    added card

--- assets/global.js
+++ assets/global.js
@@ -76,24 +76,22 @@
             {
               breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+                slidesToShow: 4,
+                slidesToScroll: 1
               }
             },
             {
-              breakpoint: 600,
+              breakpoint: 768,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2
               }
             },
             {
-              breakpoint: 480,
+              breakpoint: 300,
               settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+                slidesToShow: 2,
+                slidesToScroll: 2
               }
             }
           ]
