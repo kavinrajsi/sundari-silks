8f7c01b22c56fff38de9fdbce394a2412aca2e93
kavinrajsi <sikavinraj@gmail.com>
2022-11-09 07:40:34 +0530

    working on product image

--- assets/photoswipe-control.js
+++ assets/photoswipe-control.js
@@ -1,19 +1,5 @@
-// Include Lightbox
-var lightbox = new PhotoSwipeLightbox({
-  gallery: '.test-gallery',
-  children: 'a',
-  showHideAnimationType: 'fade',
-  pswpModule: PhotoSwipe
-});
-lightbox.init();
+// Instantiate EasyZoom instances
+var $easyzoom = $('.easyzoom').easyZoom();
 
-$('.product-images-thumbnails').on('click', 'a', function (e) {
-  var $this = $(this);
-  e.preventDefault();
-  console.log($this.attr('href'));
-  $this.parents('.product-images').find('.ImageMe a').addClass('ter').attr('href', $this.attr('href'));
-  $this.parents('.product-images').find('.ImageMe a').addClass('ter').data('pswp-width', $this.data('pswp-width'));
-  $this.parents('.product-images').find('.ImageMe a').addClass('ter').data('pswp-height', $this.data('pswp-height'));
-  $this.parents('.product-images').find('.ImageMe img').addClass('ter').attr('src', $this.attr('href'));
-});
-
+// Get an instance API
+var api = $easyzoom.data('easyZoom');
