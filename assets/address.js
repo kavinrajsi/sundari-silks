93b1358bf972764eefbda598a1f0458e6b69d00e
kavinrajsi <sikavinraj@gmail.com>
2023-01-28 18:29:45 +0530

    removed saree page, changed wishlist page template name, working on address ui

--- assets/address.js
+++ assets/address.js
@@ -1,90 +1,17 @@
-const selectors = {
-  customerAddresses: '[data-customer-addresses]',
-  addressCountrySelect: '[data-address-country-select]',
-  addressContainer: '[data-address]',
-  toggleAddressButton: 'button[aria-expanded]',
-  cancelAddressButton: 'button[type="reset"]',
-  deleteAddressButton: 'button[data-confirm-message]'
-};
+let addressClicked, addressClickedForm, addressClickedFormId = '';
 
-const attributes = {
-  expanded: 'aria-expanded',
-  confirmMessage: 'data-confirm-message'
-};
-
-class CustomerAddresses {
-  constructor() {
-    this.elements = this._getElements();
-    if (Object.keys(this.elements).length === 0) return;
-    this._setupCountries();
-    this._setupEventListeners();
+$('[data-address] button').click(function(e) {
+  e.preventDefault();
+  addressClicked = $(this).data('contact-address');
+  addressClickedForm = $(this).parent().parent().find('.contact-address-form-edit');
+  addressClickedFormId = $(this).parent().parent().find('.contact-address-form-edit').data('contact-address-form');
+  console.log(addressClicked);
+  console.log(addressClickedFormId);
+  if(addressClicked == addressClickedFormId){
+    addressClickedForm.show();
+    console.log('if');
+  }else{
+    addressClickedForm.hide();
+    console.log('else');
   }
-
-  _getElements() {
-    const container = document.querySelector(selectors.customerAddresses);
-    return container ? {
-      container,
-      addressContainer: container.querySelector(selectors.addressContainer),
-      toggleButtons: document.querySelectorAll(selectors.toggleAddressButton),
-      cancelButtons: container.querySelectorAll(selectors.cancelAddressButton),
-      deleteButtons: container.querySelectorAll(selectors.deleteAddressButton),
-      countrySelects: container.querySelectorAll(selectors.addressCountrySelect)
-    } : {};
-  }
-
-  _setupCountries() {
-    if (Shopify && Shopify.CountryProvinceSelector) {
-      // eslint-disable-next-line no-new
-      new Shopify.CountryProvinceSelector('AddressCountryNew', 'AddressProvinceNew', {
-        hideElement: 'AddressProvinceContainerNew'
-      });
-      this.elements.countrySelects.forEach((select) => {
-        const formId = select.dataset.formId;
-        // eslint-disable-next-line no-new
-        new Shopify.CountryProvinceSelector(`AddressCountry_${formId}`, `AddressProvince_${formId}`, {
-          hideElement: `AddressProvinceContainer_${formId}`
-        });
-      });
-    }
-  }
-
-  _setupEventListeners() {
-    this.elements.toggleButtons.forEach((element) => {
-      element.addEventListener('click', this._handleAddEditButtonClick);
-    });
-    this.elements.cancelButtons.forEach((element) => {
-      element.addEventListener('click', this._handleCancelButtonClick);
-    });
-    this.elements.deleteButtons.forEach((element) => {
-      element.addEventListener('click', this._handleDeleteButtonClick);
-    });
-  }
-
-  _toggleExpanded(target) {
-    target.setAttribute(
-      attributes.expanded,
-      (target.getAttribute(attributes.expanded) === 'false').toString()
-    );
-  }
-
-  _handleAddEditButtonClick = ({ currentTarget }) => {
-    this._toggleExpanded(currentTarget);
-  }
-
-  _handleCancelButtonClick = ({ currentTarget }) => {
-    this._toggleExpanded(
-      currentTarget
-        .closest(selectors.addressContainer)
-        .querySelector(`[${attributes.expanded}]`)
-    )
-  }
-
-  _handleDeleteButtonClick = ({ currentTarget }) => {
-    // eslint-disable-next-line no-alert
-    if (confirm(currentTarget.getAttribute(attributes.confirmMessage))) {
-      Shopify.postLink(currentTarget.dataset.target, {
-        parameters: { _method: 'delete' },
-      });
-    }
-  }
-}
+});
